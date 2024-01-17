c74b8ec606d59d2738e9a385bd9e2563fef4124d
x1210x <x1210x@gmail.com>
2024-01-17 15:41:22 +0900

    상세화면 갔다 올 때 스크롤 포지션 깨지는 이슈 (TODO : debounce)

--- app/src/main/java/kr/pe/ssun/carrot/ui/home/HomeViewModel.kt
+++ app/src/main/java/kr/pe/ssun/carrot/ui/home/HomeViewModel.kt
@@ -6,30 +6,31 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import kr.pe.ssun.carrot.data.SearchBookPagingSource
 import kr.pe.ssun.carrot.data.repository.BookRepository
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    repository: BookRepository,
+    private val repository: BookRepository,
 ) : ViewModel() {
 
-    private val query = MutableStateFlow("")
+    private var _books = Pager(
+        config = PagingConfig(pageSize = 20)
+    ) { SearchBookPagingSource(repository, "") }
+    .flow.cachedIn(viewModelScope)
+    val books get() = _books
 
-    val books = query.debounce(300)
-        .map { query ->
-            Pager(
-                config = PagingConfig(pageSize = 20)
-            ) { SearchBookPagingSource(repository, query) }
-                .flow.cachedIn(viewModelScope).first()
-        }
-
-    fun search(query: String) {
-        this@HomeViewModel.query.value = query
+    fun search(query: String) = viewModelScope.launch {
+        // TODO : debounce 걸기
+        Timber.d("[sunchulbaek] search = $query xxx")
+        // query를 StateFlow로 만들어서 상세화면 진입 후 돌아왔을 때, 다시 Pager가 생성되는 이슈가 있어서
+        // search에 Pager 생성하도록 변경 (상세 진입 후 스크롤 포지션 깨짐 vs 매번 api 호출)
+        _books = Pager(
+            config = PagingConfig(pageSize = 20)
+        ) { SearchBookPagingSource(repository, query) }
+            .flow.cachedIn(viewModelScope)
     }
 }
